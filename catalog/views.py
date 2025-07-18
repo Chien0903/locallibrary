bb5531ab0abc5c193291ff014b677eb97814ebaf
iselab-dorlab <iselab.bkai@gmail.com>
2025-07-18 16:06:14 +0700

    Django tutorial Part7: Session framework

--- catalog/views.py
+++ catalog/views.py
@@ -24,6 +24,7 @@ def index(request):
     num_visits = request.session.get('num_visits', 1)
     request.session['num_visits'] = num_visits + 1
     
+    
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
